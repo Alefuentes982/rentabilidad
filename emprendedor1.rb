7cd621a39d7c65c224fc3bf49a8f71ceeadf9908
Ale Fuentes <jano231982@gmail.com>
2020-10-15 15:19:17 -0300

    final2.0

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -3,9 +3,11 @@
  usuarios = ARGV[1].to_i
  gastos = ARGV[2].to_i
 
-utilidades = ((preciov * usuarios) - gastos)
-impuesto = ((utilidades * 35) / 100).to_f
+utilidades = (preciov * usuarios) - gastos
+impuesto = (utilidades * 35) / 100.to_f
 total = (utilidades - impuesto).to_f
+
+puts impuesto
 
 if utilidades > 0 
   puts "el total de utilidades es:#{total}"
